1a34441b955cb5ab467c159f9fdb9f650f6a59df
MartinMwiti <martinmwiti007@gmail.com>
2020-09-22 17:31:13 +0300

    update

---
+++ Dart/quotes/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'quote.dart'
+
+void main() {
+  runApp(MaterialApp(home: QuoteList()));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+  // The framework calls createState the first time a widget
+  // appears at a given location in the tree.
+  // If the parent rebuilds and uses the same type of
+  // widget (with the same key), the framework re-uses the State object
+  // instead of creating a new State object.
+}
+
+// class object
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Oscar', text: "The way to get started is to quit talking and begin doing."),
+    Quote(author: 'Oscar', text: "All our dreams can come true, if we have the courage to pursue them."),
+    Quote(author: 'Oscar', text: "Your time is limited, so don't waste it living someone else's life."), 
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // This method(build) is rerun every time setState is called,
+    // The BuildContext indicates where the build is
+    // taking place and therefore which theme to use.
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        // map fun will cycle through list a list of data & perform a func to each item on the list.
+        // children expect a list. So use 'toList()' to turn the iterable to list
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      ),
+    );
+  }
+}
---
+++ Dart/quotes/lib/quote.dart
@@ -0,0 +1,9 @@
+// class is a blueprint for an object i.e it's properties
+class Quote {
+  // property
+  String text;
+  String author;
+
+  // contructor(named parameter) - takes in values. SHORTER
+  Quote({this.text, this.author});
+}
