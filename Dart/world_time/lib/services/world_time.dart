578fe8bd702b7f642667d0a41605007051464e47
MartinMwiti <martinmwiti007@gmail.com>
2020-09-23 17:31:29 +0300

    error handling

--- Dart/world_time/lib/services/world_time.dart
+++ Dart/world_time/lib/services/world_time.dart
@@ -13,23 +13,27 @@ class WorldTime {
   // Make request
   // Future(like promises) is a temporary placeholder value until func is complete. In this case, it's saying, it'll return a void but only after the async func is fully complete.
   Future<void> getTime() async {
-    // store value inside Response object. Response is provided by the http module. sort of 'res'
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    // print(data);
+    try {
+      // store value inside Response object. Response is provided by the http module. sort of 'res'
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    // get properties from data
-    String datetime = data['utc_datetime'];
-    // substring. go from pos 1-3. remove the '+' or '-'
-    String offset = data['utc_offset'].substring(1, 3);
+      // get properties from data
+      String datetime = data['utc_datetime'];
+      // substring. go from pos 1-3. remove the '+' or '-'
+      String offset = data['utc_offset'].substring(1, 3);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    // set time property
-    time = now.toString();
+      // set time property
+      time = now.toString();
+    } catch (e) {
+      print('Caught error: $e');
+      time = 'could not get time data';
+    }
   }
 }
-
-
