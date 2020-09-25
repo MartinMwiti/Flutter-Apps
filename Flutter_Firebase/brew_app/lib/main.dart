b807c3b295bffcd352aa342e9c5c74605e97fbfa
MartinMwiti <martinmwiti007@gmail.com>
2020-09-25 16:12:59 +0300

    provider package

--- Flutter_Firebase/brew_app/lib/main.dart
+++ Flutter_Firebase/brew_app/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:brew_app/screens/authenticate/authenticate.dart';
+import 'package:brew_app/screens/wrapper.dart';
+import 'package:brew_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_app/models/user.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     home: Authenticate(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
-
--- Flutter_Firebase/brew_app/lib/screens/wrapper.dart
+++ Flutter_Firebase/brew_app/lib/screens/wrapper.dart
@@ -1,9 +1,14 @@
 import 'package:brew_app/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_app/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // accessing user data from the provider. type of data is User
+    final user = Provider.of<User>(context);
+    print(user);
 
     // return either Home or Authenticate widget
     return Authenticate();
