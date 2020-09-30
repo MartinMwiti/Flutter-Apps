5302b17d6442a4c8ceff053edeaa9a48ebd8b2e1
MartinMwiti <martinmwiti007@gmail.com>
2020-09-30 16:17:30 +0300

    reorganizing product widget

---
+++ ecommerce/furnitureApp/lib/screens/home/components/product_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:furnitureApp/components/title_text.dart';
+import 'package:furnitureApp/constants.dart';
+import 'package:furnitureApp/models/Product.dart';
+import 'package:furnitureApp/size_config.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+    this.product,
+    this.press,
+  }) : super(key: key);
+
+  final Product product;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        // extracted a widget named Products
+        width: defaultSize * 14.5, //145
+        decoration: BoxDecoration(
+            color: kSecondaryColor,
+            borderRadius: BorderRadius.circular(defaultSize * 3) //30
+            ),
+        child: AspectRatio(
+          aspectRatio: 0.693, // dictates the dimension of the container
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1, // dictates the dimension of the image
+                child: new FadeInImage.assetNetwork(
+                  placeholder: 'assets/pinner.gif',
+                  image: product.image,
+                  fit: BoxFit
+                      .cover, // BoxFit cover - As small as possible while still covering the entire target box.
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: defaultSize),
+                child: TitleText(title: product.title),
+              ),
+              SizedBox(height: defaultSize / 2),
+              Text('\ksh ${product.price}'),
+              //Spacer creates an adjustable, empty spacer that can be used to tune the spacing between widgets in a Flex container, like Row or Column.
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
