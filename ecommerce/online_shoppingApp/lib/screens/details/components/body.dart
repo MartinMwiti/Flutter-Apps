bc18cc77acf112ca1139c27416c4297abab27401
MartinMwiti <martinmwiti007@gmail.com>
2020-10-04 12:11:09 +0300

    update

--- ecommerce/online_shoppingApp/lib/screens/details/components/body.dart
+++ ecommerce/online_shoppingApp/lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_shoppingApp/constraits.dart';
 import 'package:online_shoppingApp/models/Product.dart';
 import 'package:online_shoppingApp/screens/details/components/product_title_with_image.dart';
 
@@ -20,13 +21,38 @@ class Body extends StatelessWidget {
             child: Stack(
               children: [
                 Container(
-                  margin: EdgeInsets.only(top: size.height * 0.35),
-                  height: 500,
+                  margin: EdgeInsets.only(top: size.height * 0.38),
+                  padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPaddin,
+                      right: kDefaultPaddin),
+                  // height: 500,
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
                           topRight: Radius.circular(24))),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Color'),
+                              Row(
+                                children: [
+                                  ColorDot(color: Color(0xFF356C95), isSelected: true),
+                                  ColorDot(color: Color(0xFFF8C078)),
+                                  ColorDot(color: Color(0xFFA29B9B)),
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
                 ProductTitleWithImage(product: product)
               ],
@@ -38,3 +64,38 @@ class Body extends StatelessWidget {
   }
 }
 
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+  const ColorDot({
+    Key key,
+    this.color,
+    // By default isSelected is false
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Extracted Widget
+      margin: EdgeInsets.only(
+        top: kDefaultPaddin / 4,
+        right: kDefaultPaddin / 2,
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        // Border - A uniform border with all sides the same color and width.
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
+        shape: BoxShape.circle,
+      ),
+      child: DecoratedBox(
+        // DecoratedBox -  Creates a widget that paints a [Decoration].
+        decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      ),
+    );
+  }
+}
