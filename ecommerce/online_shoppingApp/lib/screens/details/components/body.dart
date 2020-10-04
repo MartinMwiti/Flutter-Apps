4e0b42fee298fd3d16eac0751f3a5b4028ca1d3b
MartinMwiti <martinmwiti007@gmail.com>
2020-10-04 07:42:19 +0300

    detail page update

--- ecommerce/online_shoppingApp/lib/screens/details/components/body.dart
+++ ecommerce/online_shoppingApp/lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_shoppingApp/constraits.dart';
 import 'package:online_shoppingApp/models/Product.dart';
 
 class Body extends StatelessWidget {
@@ -22,8 +23,57 @@ class Body extends StatelessWidget {
                   margin: EdgeInsets.only(top: size.height * 0.35),
                   height: 500,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(24), topRight: Radius.circular(24))
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24))),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Aristocratic Hand Bag',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        product.title,
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: kDefaultPaddin * 2.8),
+                      Row(
+                        children: [
+                          // The RichText widget displays text that uses multiple different styles.
+                          // TextSpan, which is used to describe the text in a paragraph.
+                          RichText(
+                              text: TextSpan(
+                            children: [
+                              TextSpan(text: "Price\n"),
+                              TextSpan(
+                                text: "\Ksh ${product.price}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline4
+                                    .copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                              ),
+                            ],
+                          )),
+                          SizedBox(width: kDefaultPaddin),
+                          Expanded(
+                            child: Image.asset(
+                              product.image,
+                              fit: BoxFit.fill,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 )
               ],
