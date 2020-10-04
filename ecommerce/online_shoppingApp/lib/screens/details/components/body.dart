2179875b19d5dfa337065e1b3e5e9159a2f7a2d6
MartinMwiti <martinmwiti007@gmail.com>
2020-10-04 12:18:48 +0300

    update

--- ecommerce/online_shoppingApp/lib/screens/details/components/body.dart
+++ ecommerce/online_shoppingApp/lib/screens/details/components/body.dart
@@ -35,20 +35,32 @@ class Body extends StatelessWidget {
                   child: Column(
                     children: [
                       Row(
-                        children: [
+                        children: <Widget>[
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text('Color'),
                               Row(
                                 children: [
-                                  ColorDot(color: Color(0xFF356C95), isSelected: true),
+                                  ColorDot(
+                                      color: Color(0xFF356C95),
+                                      isSelected: true),
                                   ColorDot(color: Color(0xFFF8C078)),
                                   ColorDot(color: Color(0xFFA29B9B)),
                                 ],
                               )
                             ],
-                          )
+                          ),
+                          RichText(
+                              text: TextSpan(
+                                  style: TextStyle(color: kTextColor),
+                                  children: [
+                                TextSpan(text: "Size\n"),
+                                TextSpan(
+                                  text: "${product.size}",
+                                  style: Theme.of(context).textTheme.headline5
+                                )
+                              ])),
                         ],
                       )
                     ],
